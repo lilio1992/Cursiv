5ab33106861507e60d41506f9425f510b49f1fbc
virgilio@hostienda.com <lilio2626>
2020-02-07 15:14:07 -0400

    viernes 7 febrero 3 30pm

---
+++ src/components/metodos.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container mt-5">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "metodos",
+  props: {},
+  data() {
+    return {
+    
+    };
+  },
+  methods: {
+    },
+    created(){},
+    beforeMount(){},
+    mounted(){},
+    beforeUpdate(){},
+    updated(){},
+    beforeUpdate(){
+        
+    }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
